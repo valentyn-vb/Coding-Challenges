ada53828f43135111c28020a97e4b2f9fd16eecf
Valentyn Beznosko <valikbeznosko@gmail.com>
2026-04-16 13:57:19 +0200

    commit 2-3-4 task

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+//Who’s Online?
+
 const USERS: UsersI[] = [
   {
     username: "David",
@@ -30,10 +32,9 @@ interface SORTED_USERS {
 
 type status = "online" | "offline";
 
-function sortUserByStatus() {
-  return USERS.reduce((acc, user) => {
+function sortUserByStatus(users: UsersI[]) {
+  return users.reduce((acc, user) => {
     const { status, lastActivity } = user;
-    console.log("🚀 ~ sortUserByStatus ~ lastActivity:", lastActivity);
     if (status === "online") {
       lastActivity <= 10
         ? (acc.online = [...(acc.online || []), user.username])
@@ -48,4 +49,123 @@ function sortUserByStatus() {
   }, {} as SORTED_USERS);
 }
 
-console.log("🚀 ~sortUserByStatus():", sortUserByStatus());
+console.log("🚀 ~sortUserByStatus():", sortUserByStatus(USERS));
+
+console.warn("////////////////////////");
+// Who’s Online? end
+
+// Credit Card Mask
+
+function maskValue(value: string) {
+  if (value.length <= 4) return value;
+
+  let unVisiblePart = "";
+  let visiblePart = value.substring(value.length - 4, value.length + 1);
+
+  for (let i = 1; i <= value.length - 4; i++) {
+    unVisiblePart += "#";
+  }
+
+  return {
+    originalValue: value,
+    displayedValue: unVisiblePart + visiblePart,
+  };
+}
+
+console.log(maskValue("4556364607935616")); // "############5616"
+console.log(maskValue("64607935616")); // "#######5616"
+console.log(maskValue("1")); // "1"
+console.log(maskValue("")); // ""
+console.log(maskValue("00112233")); // "#######5616"
+console.log(maskValue("Banana")); // "#######5616"
+
+console.warn("////////////////////////");
+
+// Credit Card Mask End
+
+//Your order, please
+
+function sortString(value: string) {
+  if (!value.length) return "";
+
+  const res = value.split(" ");
+  const sortedArray = [...res];
+  res.map((word) => {
+    const letters = word.split("");
+    const index = letters.find((letter) => !!Number(letter)) as any;
+    sortedArray.splice(index - 1, 1, word);
+  });
+  console.log("🚀 ~ sortString ~ sortedArray:", sortedArray);
+
+  return sortedArray.join(" ");
+}
+console.log("sortString: ", sortString("is2 Thi1s T4est 3a"));
+console.log("sortString: ", sortString("4of Fo1r pe6ople g3ood th5e the2"));
+console.log("sortString: ", sortString(""));
+console.warn("////////////////////////");
+
+// Find the missing letter
+
+const alphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+const findTheMissingLetter = (arr) => {
+  const findRegister =
+    arr[0] === arr[0].toUpperCase() ? "toUpperCase" : "toLowerCase";
+
+  let missingLetter = "";
+
+  const findFirstLetterIndex = alphabet.findIndex(
+    (el) => el === arr[0].toUpperCase(),
+  );
+  const findLastLetterIndex = alphabet.findIndex(
+    (el) => el === arr[arr.length - 1].toUpperCase(),
+  );
+
+  const updateAlp = alphabet.slice(
+    findFirstLetterIndex,
+    findLastLetterIndex + 1,
+  );
+
+  updateAlp.map((el) => {
+    if (!arr.includes(el?.[findRegister]())) {
+      missingLetter = el?.[findRegister]();
+    }
+  });
+
+  return missingLetter;
+};
+
+console.log(findTheMissingLetter(["a", "b", "c", "d", "f"]));
+
+console.log(findTheMissingLetter(["O", "Q", "R", "S"]));
+
+console.log(findTheMissingLetter(["O", "Q"]));
+
+console.warn("//////////");
